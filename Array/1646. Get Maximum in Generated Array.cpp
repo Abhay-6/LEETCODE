6d384c85dbf631ef82479f0adddc65c158b17c5f
Abhay Kumar <abhayjha78588@gmail.com>
2026-06-02 17:31:17 +0530

    Create 1646. Get Maximum in Generated Array.cpp

---
+++ Array/1646. Get Maximum in Generated Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if(n==0){
+            return 0;
+        }
+        vector<int> nums(n+1);
+        nums[0]=0;
+        nums[1]=1;
+        for(int i=1;2*i<=n;i++){
+            nums[2*i]=nums[i];
+            if(2*i+1<=n){
+                nums[2*i+1]=nums[i]+nums[i+1];
+            }
+        }
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-1];
+    }
+};
