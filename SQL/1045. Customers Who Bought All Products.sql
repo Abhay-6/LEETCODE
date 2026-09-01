58d6228b8bd60efc55f7a1c0633469b1f259bdf5
Abhay Kumar <abhayjha78588@gmail.com>
2026-09-02 00:32:14 +0530

    Create 1045. Customers Who Bought All Products.sql

---
+++ SQL/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,9 @@
+select distinct c.customer_id
+from customer c
+join Product p
+on c.product_key=p.product_key
+group by c.customer_id
+having count(distinct c.product_key) = (
+    select count(*)
+    from Product
+)
